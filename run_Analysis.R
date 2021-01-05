335c3b5b1a7f2a2057e8dc3e13e3c409115a9855
fkreuzer <75771713+fkreuzer@users.noreply.github.com>
2021-01-05 18:51:44 +0100

    add subject and ydata

--- run_Analysis.R
+++ run_Analysis.R
@@ -2,6 +2,7 @@
 
 library(readr)
 library(stringr)
+library(dplyr)
 
 #define filepaths
 
@@ -9,6 +10,13 @@ filepath.test = "./UCI_HAR_Dataset/test/X_test.txt"
 filepath.train = "./UCI_HAR_Dataset/train/X_train.txt"
 filepath.testp = "./UCI_HAR_Dataset/test/X_test_p.txt"
 filepath.trainp = "./UCI_HAR_Dataset/train/X_train_p.txt"
+
+filepath.testy = "./UCI_HAR_Dataset/test/y_test.txt"
+filepath.trainy = "./UCI_HAR_Dataset/train/y_train.txt"
+
+filepath.subjecttest = "./UCI_HAR_Dataset/test/subject_test.txt"
+filepath.subjecttrain = "./UCI_HAR_Dataset/train/subject_train.txt"
+
 
 old_filepaths = c(filepath.test,filepath.train)
 new_filepaths = c(filepath.testp,filepath.trainp)
@@ -43,13 +51,25 @@ indexfeatures <- c(1:6,41:46,81:86,121:126,161:166,201,202,214,215,227,228,
 featurenames <- read.delim("./UCI_HAR_Dataset/features.txt",
                        header = FALSE,sep = " ")
 
-#read in the data
+#read in the Xdata
 
 Xtest <- read.delim(new_filepaths[[1]],sep = " ", header = FALSE)
 Xtrain <- read.delim(new_filepaths[[2]],sep = " ", header = FALSE)
 
 Xtest <- Xtest[2:562]
 Xtrain <- Xtrain[2:562]
+
+#read in the ydata
+
+ytest <- read.delim(filepath.testy, sep = " ",header = FALSE)
+ytrain <- read.delim(filepath.trainy, sep = " ",header = FALSE)
+
+#read in the subject data
+
+subjecttest <- read.delim(filepath.subjecttest, sep = " ",header = FALSE)
+subjecttrain <- read.delim(filepath.subjecttrain, sep = " ",header = FALSE)
+
+
 
 #build the dataframe
 
@@ -58,3 +78,12 @@ Xtrain <- Xtrain[,indexfeatures]
 
 names(Xtest) <- featurenames[indexfeatures,2]
 names(Xtrain) <- featurenames[indexfeatures,2]
+
+#transform to tibbles
+
+Xtest <- as_tibble(Xtest)
+Xtrain <- as_tibble(Xtrain)
+
+#add features and results
+Xtest <- mutate(Xtest, y = ytest[1], subject = subjecttest[1])
+Xtrain <- mutate(Xtrain, y = ytrain[1], subject = subjecttrain[1])
